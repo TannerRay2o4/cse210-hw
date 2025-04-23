cb52c16bc8cead79230bb025902ba27c83f5179b
Tanner Ray <tannerray2o4@gmail.com>
2025-04-23 17:35:03 -0600

    Changed main function to compute the area of a circle.

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,9 +1,22 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        // This is a program to compute the area of a circle.
+
+        // Get the radius from the user.
+        Console.Write("\nPlease enter the radius: ");
+        string text = Console.ReadLine();
+        double radius = double.Parse(text);
+
+        // Compute the area of the circle
+        double area = Math.PI * radius * radius;
+
+        // Display the area for the user to see.
+        Console.WriteLine($"\nThe area of the circle is: {area} \n");
+     
     }
 }
