9765434d5d28a66bae102c14153552880d9b2963
Tanner Ray <tannerray2o4@gmail.com>
2025-04-23 20:26:46 -0600

    Letter Grade Calculator

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,48 @@
 using System;
 
+// Tanner Ray
+// CSE 210
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("\nWhat is your grade percentage? ");
+        string answer = Console.ReadLine();
+        int percent = int.Parse(answer);
+
+        string letter = "";
+
+        if (percent >= 90)
+        {
+            letter = "A";
+        }
+        else if (percent >= 80)
+        {
+            letter = "B";
+        }
+        else if (percent >= 70)
+        {
+            letter = "C";
+        }
+        else if (percent >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"\nYour grade is: {letter} ");
+
+        if (percent >= 70)
+        {
+            Console.WriteLine("You passed the course! ");
+        }
+        else
+        {
+            Console.WriteLine("You recieved a grade lower than a 70% which means you didn't pass the class. ");
+        }
     }
 }
