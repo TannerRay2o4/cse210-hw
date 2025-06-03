25025295fb0c455b1418595bf2cedb69a54b19df
Tanner Ray <tannerray2o4@gmail.com>
2025-06-03 07:22:52 -0600

    Learning Activity 4

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        // Create a base "Assignment" object
+        Assignment a1 = new Assignment("\n\nDale Shaw", "Multiplication\n");
+        Console.WriteLine(a1.GetSummary());
+
+        // Now create the derived class assignments
+        MathAssignment a2 = new MathAssignment("Tanner Ray", "Integration", "12.4", "6, 8, 10, 12 \n");
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetHomeworkList());
+
+        WritingAssignment a3 = new WritingAssignment("Dillon Hatfield", "American History", "The Forever War");
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.GetWritingInformation());
     }
 }
